6e07a51072ab2b341c220b23d98850a8c5f4d107
Stefan Vitoria <seacod3@sapo.pt>
2023-07-24 13:55:21 -0100

    fix: code refactor

--- src/components/sanitizeDecimal.ts
+++ src/components/sanitizeDecimal.ts
@@ -1,23 +1,3 @@
-// const sanitizeDecimal = (number: string): string => {
-//     // Initialize a sanitized number string.
-//     let sanitizedNumber = "";
-
-//     // Loop through the characters in the input number.
-//     for (const character of number) {
-//         // If the character is not a decimal point, add it to the sanitized number.
-//         if (character !== ".") {
-//             sanitizedNumber += character;
-//         }
-//         // If the character is a decimal point, and there is not already a decimal point in the sanitized number, add it.
-//         else if (sanitizedNumber.indexOf(".") === -1) {
-//             sanitizedNumber += character;
-//         }
-//     }
-
-//     // Return the sanitized number.
-//     return sanitizedNumber;
-// }
-
 
 const sanitizeDecimal = (expression: string): string => {
     // Regular expression to match numbers with multiple decimal points
@@ -28,7 +8,6 @@ const sanitizeDecimal = (expression: string): string => {
 
     return sanitizedExpression;
 };
-
 
 
 export default sanitizeDecimal;
